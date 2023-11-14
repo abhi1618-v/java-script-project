c4e30086a845e7547f94d6d19f253a7ec0887fb9
abhishek vishwakarma <114329164+abhi1618-v@users.noreply.github.com>
2023-11-14 12:32:19 +0530

    first project

---
+++ colorSwitcherProject/script.js
@@ -0,0 +1,27 @@
+const button = document.querySelectorAll(".button");
+const body = document.querySelector('.main')
+console.log(body)
+
+button.forEach(function (button) {
+    button.addEventListener('click', function (e) {
+        console.log(e)
+        console.log(e.target)
+        if(e.target.id === 'blue'){
+            body.style.backgroundColor = e.target.id
+        }
+        
+        if(e.target.id === 'green'){
+            body.style.backgroundColor = e.target.id
+        }
+
+        if(e.target.id === 'red'){
+            body.style.backgroundColor = e.target.id
+        }
+
+        if(e.target.id === 'yellow'){
+            body.style.backgroundColor = e.target.id
+        }
+
+    })
+})
+    
